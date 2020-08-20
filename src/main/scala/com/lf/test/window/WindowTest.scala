75d5739cc978d28fb9d95afe3d004979b67ddd00
apple <fly.liu@dbappsecurity.com.cn>
2020-08-20 18:30:08 +0800

    window function

--- src/main/scala/com/lf/test/window/WindowTest.scala
+++ src/main/scala/com/lf/test/window/WindowTest.scala
@@ -1,7 +1,17 @@
 package com.lf.test.window
 
+import java.util.Random
+
 import com.lf.test.source.SensorReading
+import org.apache.flink.api.common.functions.ReduceFunction
+import org.apache.flink.api.java.tuple.Tuple
+import org.apache.flink.streaming.api.functions.source.SourceFunction
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.function.WindowFunction
+import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.util.Collector
 
 /**
  * @Classname WindowTest
@@ -15,16 +25,71 @@ object WindowTest {
 
     env.setParallelism(1)
 
-    val inputStream = env.readTextFile("/path")
-    val dataStream = inputStream.map(
-      data => {
-        val dataArr = data.split(",")
-        SensorReading(dataArr(0), dataArr(1).toLong, dataArr(2).toDouble).toString
-      }
-    )
+    val inputStream = env.addSource(new MySensorSource())
 
-    val resultStream = dataStream.keyBy("id")
-      .window()
+    val resultStream = inputStream.keyBy("id")
+//      .window(EventTimeSessionWindows.withGap(Time.minutes(1))) // 会话创建
+      .timeWindow(Time.seconds(15))
+//      .reduce(new MyReduce())
+        .apply(new MyWindowFunction)
+
+    resultStream.print()
+
+    env.execute("window test")
   }
 
+}
+
+class MyReduce() extends ReduceFunction[SensorReading] {
+  override def reduce(value1: SensorReading, value2: SensorReading): SensorReading = {
+    SensorReading(value1.id, value1.timestamp.max(value2.timestamp), value1.temp.min(value2.temp))
+
+  }
+}
+
+class MySensorSource() extends SourceFunction[SensorReading] {
+
+  var running : Boolean = true
+
+  // 随机生成数据
+  override def run(ctx: SourceFunction.SourceContext[SensorReading]): Unit = {
+
+    // 随机数
+    val random = new Random()
+
+    // 随机生成10个传感器的温度值，并且不停在之前的温度基础上更逊（随机上下波动）
+    // 首先生成10个传感器的初始温度
+    var curTemps = 1.to(10).map(
+      i => ("sensor_" + i, 60 + random.nextGaussian() * 20)
+    )
+
+    // 生成数据
+    while (running) {
+      // 在当前温度基础上随机生成微小波动
+      curTemps = curTemps.map(
+        data => (data._1, data._2 + random.nextGaussian())
+      )
+
+      // 获取当前时间
+      val curTs = System.currentTimeMillis()
+
+      // 包装成样例类
+      curTemps.foreach(
+        data => ctx.collect(SensorReading(data._1, curTs, data._2))
+      )
+
+      // 间隔时间
+      Thread.sleep(1000L)
+    }
+  }
+
+  override def cancel(): Unit = running = false
+}
+
+// 自定义一个全窗口函数
+class MyWindowFunction extends WindowFunction[SensorReading, (Long, Int), Tuple, TimeWindow] {
+  override def apply(key: Tuple, window: TimeWindow, input: Iterable[SensorReading], out: Collector[(Long, Int)]): Unit = {
+
+    out.collect((window.getStart, input.size))
+  }
 }
